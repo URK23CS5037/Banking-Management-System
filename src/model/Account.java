16898ce29999ba637b1352f7d465107334a349ff
URK23CS5037 <153484671+URK23CS5037@users.noreply.github.com>
2026-08-07 22:55:01 +0530

    Add Account class with properties and methods

--- src/model/Account.java
+++ src/model/Account.java
@@ -1,1 +1,84 @@
+package model;
 
+public class Account {
+
+    private int accountNumber;
+    private int customerId;
+    private String accountType;
+    private double balance;
+    private String pin;
+    private String status;
+    private int failedAttempts;
+
+    // Default Constructor
+    public Account() {
+    }
+
+    // Constructor
+    public Account(int customerId,
+                   String accountType,
+                   String pin) {
+
+        this.customerId = customerId;
+        this.accountType = accountType;
+        this.pin = pin;
+    }
+
+    // Getters and Setters
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getFailedAttempts() {
+        return failedAttempts;
+    }
+
+    public void setFailedAttempts(int failedAttempts) {
+        this.failedAttempts = failedAttempts;
+    }
+}
