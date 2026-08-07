cbb426ddea223364b0b1c69f925e039e46464b0e
URK23CS5037 <153484671+URK23CS5037@users.noreply.github.com>
2026-08-07 22:52:50 +0530

    DBConnection.java
    Implement database connection logic for MySQL.

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -1,1 +1,31 @@
 
+
+package database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static final String URL =
+        "jdbc:mysql://localhost:3306/banking_management_system";
+
+    private static final String USER = "root";
+    private static final String PASSWORD = "YOUR_PASSWORD";
+
+    public static Connection getConnection() {
+
+        try {
+            Connection con =
+                DriverManager.getConnection(URL, USER, PASSWORD);
+
+            //System.out.println("DB Connected");
+            return con;
+
+        } catch (Exception e) {
+            System.out.println("Database Connection Failed");
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
